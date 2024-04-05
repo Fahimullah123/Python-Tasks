c8227d71c756efee1a5e64b965bc2c196d27f661
Fahim Ullah <131962266+mrcoder19@users.noreply.github.com>
2024-04-05 14:13:38 +0500

    Greating message

---
+++ Task no 5.py
@@ -0,0 +1,19 @@
+#  Create a program that takes a user's name and age as 
+#  input and prints a greeting message
+
+
+#<--------- Solution ----------->
+
+
+# create a function for greating message
+
+def greating(name,age):
+
+    print(f"Hello! {name} your are {age} year old")
+
+
+
+name=input("Enter your Name : ")
+age=input("Enter your Age : ")
+
+greating(name,age)
