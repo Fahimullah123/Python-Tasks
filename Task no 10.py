ee47f26a5012ead0f1ca5f4d7b60efe3cfe8261b
Fahim Ullah <131962266+mrcoder19@users.noreply.github.com>
2024-04-15 09:55:25 +0500

    sum of all positive numbers

--- Task no 10.py
+++ Task no 10.py
@@ -1,17 +1,23 @@
 # Q 10: Given a list of integers, find the sum of all positive numbers
 
 
-def sum_of_list(list):
+
+def sum_of_positive(list):
+
+    sum=0
 
     for i in list:
+        if i>0:
+            sum=sum+i
 
-        sum= sum+i
 
-        return sum
-    
+    return sum
+   
 
-list=[45,8,6,8,64,95,1]
+list=[-45,8,-6,10,-64,95,1]
 
-result=sum_of_list(list)
+result=sum_of_positive(list)
 
-print("The sum of is : ",sum)
+print("The sum of the list is : ", result)
+
+
