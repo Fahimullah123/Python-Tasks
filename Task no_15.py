a217382e65ee19f95c46885faefe2b7eb4df6b2c
Fahim Ullah <131962266+mrcoder19@users.noreply.github.com>
2024-04-15 11:16:20 +0500

    list of fruits and access elements using indexing

---
+++ Task no_15.py
@@ -0,0 +1,14 @@
+#Q 3: Create a list of fruits and access elements using indexing
+
+
+fruit_list=['Apple', 'Mango', 'Banana', 'Lemon']
+
+
+first_fruit=fruit_list[0]
+second_fruit=fruit_list[1]
+third_fruit=fruit_list[2]
+
+
+print("First Fruit : ",first_fruit)
+print("Second Fruit : ",second_fruit)
+print("Third Fruit : ", third_fruit)
