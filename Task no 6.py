6df621ad8c9d168d0c7f9a618e80f541db8fc23b
Fahim Ullah <131962266+mrcoder19@users.noreply.github.com>
2024-04-15 10:56:42 +0500

    check if a number is even or odd

---
+++ Task no 6.py
@@ -0,0 +1,15 @@
+
+# Q6: Write a program to check if a number is even or odd
+
+# first we creat list
+
+list=[12,13,14,34,34,35,37]
+
+
+# now check conditiion  if its even or odd
+
+for i in list:
+    if (i%2==0):
+        print("That is Even number",i)
+    else:
+        print("That is odd number",i)
