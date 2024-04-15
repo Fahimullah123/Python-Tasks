fede6048c63e920fab6bcc1cb79420631572e459
Fahim Ullah <131962266+mrcoder19@users.noreply.github.com>
2024-04-15 10:23:14 +0500

    variables for storing a person's name, age, and average test score

---
+++ Task no 13.py
@@ -0,0 +1,12 @@
+#Q 1:  Create variables for storing a person's name, age, and average test score
+
+
+#Varibales
+
+P_name='Fahim Ullah'
+P_age=24
+Test_score=55.7
+
+print("Name : ",P_name)
+print("Age : ",P_age)
+print("Test_score : ",Test_score)
