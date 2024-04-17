0a243e588ae71ebeb54d4570d342f4cee52e197f
Fahim Ullah <131962266+mrcoder19@users.noreply.github.com>
2024-04-17 18:21:42 +0500

    unction to reverse a given strings

---
+++ Task no_19.py
@@ -0,0 +1,9 @@
+#Create a function to reverse a given strings
+
+def string_reverce(string):
+    return string[::-1]
+
+
+string="Hello this is Fahim"
+print("Org string : ",string)
+print("Reverse String : ",string_reverce(string))
