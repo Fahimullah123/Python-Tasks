16c394c75ec5ef1cd513907e7739e99879376448
Fahim Ullah <131962266+mrcoder19@users.noreply.github.com>
2024-04-17 18:20:52 +0500

    names, concatenate

---
+++ Task no_20.py
@@ -0,0 +1,9 @@
+# Given a list of names, concatenate them into a single string separated by spaces
+
+def name_concatenate(List):
+    return " ".join(List)
+
+
+
+List=['Fahim','Raheem','Musa','Ahmad']
+print(name_concatenate(List))
