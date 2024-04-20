6aca99ec9c05f9ffe3afcec4cca60d18c026d68f
Fahim Ullah <131962266+mrcoder19@users.noreply.github.com>
2024-04-20 07:34:38 +0500

    if a number is prime.

--- Task no_24.py
+++ Task no_24.py
@@ -1,17 +1,22 @@
 #  Write a program to check if a number is prime.
 
 
-def check_number(number):
+def is_prime(number):
+    if number <= 1:
+        return False  # 1 and numbers less than 1 are not prime
+    if number <= 3:
+        return True  # 2 and 3 are prime numbers
 
-    if number%2==0:
-        print("That is Even Number : ",number)
-    else:
-        print("That is Odd Number : ",number)
+    # Check if the number is divisible by any integer from 2 to the square root of the number
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False  # If it's divisible by any number, it's not prime
+    return True
 
-number=int(input('Enter Any Number : '))
-
-
-#call the function
-check_number(number)
-
+# Example usage:
+num = 17
+if is_prime(num):
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
 
