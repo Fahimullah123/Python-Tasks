426f9b6f79b05ee5a0b922efa5629b2c69122ade
Fahim Ullah <131962266+mrcoder19@users.noreply.github.com>
2024-05-03 22:09:53 +0500

    concatenate them into a single list

---
+++ Task no_42.py
@@ -0,0 +1,12 @@
+# Given two lists of numbers, concatenate them into a single list
+
+def concatenate(list1,list2):
+    return list1+list2
+
+list1=[1,3,2,42,43,3]
+list2=[6,4,3,5,6,4]
+
+
+result=concatenate(list1,list2 )
+
+print("concatenage list is : ",result)
