e93aec32ee8ee5e306f82e9e22f8d76befe8f774
Fahim Ullah <131962266+mrcoder19@users.noreply.github.com>
2024-05-13 10:11:42 +0500

    helo

---
+++ Task no_26.py
@@ -0,0 +1,22 @@
+# Q 2: Create a loop that prints the first 10 even numbers.
+
+for i in range(0,20):
+    if i%2==0:
+        print(i)
+
+
+
+# 2nd method
+
+count=0
+number=0
+
+while count<10:
+    if number%2==0:
+        print(number)
+        count+=1
+    number+=1
+
+
+
+
